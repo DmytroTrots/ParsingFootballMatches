eb16e067431da364f45901d7d4aabff2d364d124
DmytroTrots <dtrots27@gmail.com>
2021-03-10 21:59:02 +0200

    dataChose

--- src/main/java/siteConnection.java
+++ src/main/java/siteConnection.java
@@ -3,11 +3,17 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class siteConnection {
     public static Document getPage() throws IOException {
-        String url = "https://football.ua/scoreboard/";
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("Введите дату в формате yyyy-mm-dd");
+        String date = sc.nextLine();
+        String url = "https://football.ua/default.aspx?menu_id=scoreboard&dt=" + date;
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     };
